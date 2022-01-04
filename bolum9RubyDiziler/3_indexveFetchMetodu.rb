85744a30f4b2cbb255143db215c040af38d08473
Muhammet Abdullah Şahin <21060622@stu.omu.edu.tr>
2022-01-04 18:49:48 +0300

    main

--- bolum9RubyDiziler/3_indexveFetchMetodu.rb
+++ bolum9RubyDiziler/3_indexveFetchMetodu.rb
@@ -5,5 +5,5 @@ p isimler[50] # nil döner
 
 p isimler.fetch(0)
 p isimler.fetch(3)
-# p isimler.fetch(50) # hata verir
+# p isimler.fetch(50) # hata kontrolü yapar verir
 p isimler.fetch(50, "böyle bir öge yok") # 50 öge olmadığı için hata vermek yerine "," den sonraki ogeyi döndürür
